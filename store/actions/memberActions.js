5f2e18cdca94c1ad265c58b69254dbe6e08a9dea
trapintrovert <mcneo062@gmail.com>
2020-02-21 01:41:45 +0100

    modification

--- store/actions/memberActions.js
+++ store/actions/memberActions.js
@@ -1,32 +1,45 @@
-import actions from "./index";
-import http from "./http";
+import actions from './index';
+import http from './http';
 
 export const getMembers = () => async dispatch => {
-  const members = await http.get("/user");
+  const members = await http.get('/user');
   return dispatch({ type: actions.GET_MEMBERS, members: members.data });
 };
 
-export const regMember = ({full_name, job_title, portfolio, email_address, avatar}) => async dispatch => {
+export const regMember = ({
+  full_name,
+  job_title,
+  portfolio,
+  email_address,
+  avatar
+}) => async dispatch => {
   const config = {
     headers: {
       'Content-Type': 'application/json'
     }
-  }
-  const body = JSON.stringify({ full_name, job_title, portfolio, email_address, avatar })
+  };
+  const body = JSON.stringify({
+    full_name,
+    job_title,
+    portfolio,
+    email_address,
+    avatar
+  });
   try {
-    const res = await http.post("/user", body, config);
-    dispatch({ type: actions.REG_MEMBER, reg_member: res.data})
-  } catch(err) {
-    const error = err.response.data.message
-      console.log(error)
+    const res = await http.post('/user', body, config);
+    dispatch({ type: actions.REG_MEMBER, reg_member: res.data });
+    console.log(res.data);
+  } catch (err) {
+    const error = err.response.data.message;
+    console.log(error);
 
-      dispatch({
-        type: actions.REG_FAILURE,
-        error
-      })
+    dispatch({
+      type: actions.REG_FAILURE,
+      error
+    });
   }
-}
+};
 
-export const onMemberSearch = (text) => async dispatch =>{
-  return dispatch({ type: actions.CHANGE_SEARCHFIELD, search: search.text})
-}
+export const onMemberSearch = text => async dispatch => {
+  return dispatch({ type: actions.CHANGE_SEARCHFIELD, search: search.text });
+};
